c2469b380187aeb969922d463b88dd8af1eab69d
I P <ip@MacBook-Pro-I.local>
2026-02-26 08:43:41 +0200

    Remove tab sub-descriptions and increase spacing below headers

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // StaySharp Service Worker
 // Auto-versioned: timestamp is injected by deploy script
-const CACHE_VERSION = '1772087936';
+const CACHE_VERSION = '1772088221';
 const CACHE_NAME = `staysharp-${CACHE_VERSION}`;
 const ASSETS = [
     './',
